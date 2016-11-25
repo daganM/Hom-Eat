b722247352d3247bceb3d3d28d6a6ffe5afb124d
Dagan <costaoverblog@ymail.com>
2016-11-25 16:24:53 +0100

    Search update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,10 +6,14 @@ class PagesController < ApplicationController
   end
 
   def search
-    places = Place.where(city: "Bordeaux")
+    @lat1 = params[:lat].to_f
+    @lng1 = params[:lng].to_f
+    places = Place.all
     place_ids = []
     places.each do |p|
-      place_ids << p.id
+      if CoorDistance(p.latitude, p.longitude)
+        place_ids << p.id
+      end
     end
     @meals = Meal.where(place_id: place_ids)
   end
@@ -19,4 +23,21 @@ class PagesController < ApplicationController
 
   def contact
   end
+
+  private
+    def CoorDistance(lat2 = 0, lng2 = 0, maxDist = 1)
+      r = 6371 # in kilometers
+      lat2 = lat2.to_f
+      lng2 = lng2.to_f
+      x = (lat2 - @lat1) * Math.cos((@lng1 + lng2) / 2)
+      y = (lng2 - @lng1)
+      d = Math.sqrt(x*x + y*y)*r
+            if d <= maxDist
+              logger.info(d)
+              return true
+            else
+              logger.info(d)
+              return false
+            end
+    end
 end
