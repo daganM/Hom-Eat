e335118bf3b6fdf446f1ca5e937f92583988dc4d
Dagan <costaoverblog@ymail.com>
2017-01-13 12:17:09 +0100

    Fix issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { :host => 'homeat.heroku.com' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
